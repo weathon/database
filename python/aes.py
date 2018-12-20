2416270a0ac9c1637972ad9153e6f9d00b1d14b8
weathon <41298844+weathon@users.noreply.github.com>
2018-12-20 14:19:59 -0600

    修改加密策略

--- python/aes.py
+++ python/aes.py
@@ -2,11 +2,11 @@
 u"""
 这是主程序后端的一个模块，用于处理图片的加密解密以及hash
 创建时间：2018年12月20日13:39:39
-版本：1.0.0(版本控制已经同步至GitHub)
+版本：1.0.2(版本控制已经同步至GitHub)
 由于众多哭的支持兼容性，PY2还是PY3暂未确定
 """
 import hashlib
-def encryption(path,key):#加密
+def encryption(base64,save_path,key):#加密
 	pass
 	
 def decrypt(path,key):#解密
--- python/sql.py
+++ python/sql.py
@@ -1,25 +1,31 @@
 #coding:utf-8
 u"""
 这是主程序的数据库逻辑处理和链接程序
-创建时间：2018年12月20日13:39:39
-版本：1.0.3(版本控制已经同步至GitHub)
+创建时间：2018年12月20日13:39:39(时区：芝加哥)
+版本：1.1.0(版本控制已经同步至GitHub)
 由于众多哭的支持兼容性，PY2还是PY3暂未确定
 """
 
-def checkchar(string):#检测非法字符，防止sql注入
-	i=string
-	for i in string:
-		pass
-	return i#返回，1表示允许，0表示拒绝
-	
+def checkchar(string):#检测非法字符，防止sql注入，但不完善(由于只是怕用户自己误操作，不考虑攻击)，需谨慎使用，后期可能考虑base64
+	i=[]
+	wordlist=['"',"'","\\","and","or","="," "]#危险字符，
+	for char in wordlist:
+		if char in string:
+			i.append(char)
+		
+	return i#返回，空列表表示允许，否则表示拒绝，内容为拒绝的字符
+
 #数据库实现:SQLServer
 import pymssql
 class main():
-	def connect(server,user,password,database):
+	def connect(self,server,user,password,database):
 		# server    数据库服务器名称或IP
 		# user      用户名
 		# password  密码
 		# database  数据库名称
-		conn = pymssql.connect(server, user, password, database)
-		cursor = conn.cursor()
+		self.conn = pymssql.connect(server, user, password, database)
+		self.cursor = conn.cursor()
 	
+
+def backup():#备份数据库(不是备份图片)
+	pass
